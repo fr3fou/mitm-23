052e12fad5021b830140678a6f64d6b46f0c4e1a
Simo Aleksandrov <simo3003@me.com>
2020-08-29 17:43:30 +0300

    Go over every file

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("hi")
+	dir := ""
+	flag.StringVar(&dir, "dir", "", "path to the directory of subs")
+
+	flag.Parse()
+
+	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		// We only need the files
+		if info.IsDir() {
+			return nil
+		}
+
+		fmt.Println(path)
+		return nil
+	})
 }
