0729468568c451d0f691f8ab6c4e8037f306fcc9
Simo Aleksandrov <simo3003@me.com>
2020-08-30 03:07:42 +0300

    Parse every srt file and append every line

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/fr3fou/mitm-23
 
 go 1.15
+
+require github.com/asticode/go-astisub v0.4.1
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
+
+	"github.com/asticode/go-astisub"
 )
 
 func main() {
@@ -13,17 +16,32 @@ func main() {
 
 	flag.Parse()
 
+	lines := []string{}
+	start := time.Now()
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		// We only need the files
 		if info.IsDir() {
 			return nil
 		}
 
-		fmt.Println(path)
+		// Parse the file
+		srt, err := astisub.OpenFile(path)
+		if err != nil {
+			return err
+		}
+
+		for _, item := range srt.Items {
+			lines = append(lines, item.String())
+		}
+
 		return nil
 	})
 
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println(time.Since(start))
+	fmt.Println(len(lines))
 }
+
+// func contains([])
