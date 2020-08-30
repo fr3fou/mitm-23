98887bf9fb18976f7ad7db59979f42afbd550c12
Simo Aleksandrov <simo3003@me.com>
2020-08-30 03:45:55 +0300

    finish?

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/asticode/go-astisub"
@@ -14,6 +16,11 @@ var twentyThree = []string{
 	"23",
 	"Twenty Three",
 	"Twenty-Three",
+	"twenty three",
+	"twenty-three",
+}
+var blacklist = []string{
+	"Synchro to version",
 }
 
 func main() {
@@ -22,10 +29,14 @@ func main() {
 
 	flag.Parse()
 
-	lines := []string{}
+	subs := []astisub.Subtitles{}
 	start := time.Now()
-	log.Printf("Beginning parsing SRT files in %s", dir)
+	log.Printf("Beginning parsing SRT files in %s...", dir)
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
 		// We only need the files
 		if info.IsDir() {
 			return nil
@@ -37,15 +48,40 @@ func main() {
 			return err
 		}
 
-		for _, item := range srt.Items {
-			lines = append(lines, item.String())
-		}
+		subs = append(subs, *srt)
 
 		return nil
 	})
-
 	if err != nil {
 		panic(err)
 	}
 	log.Printf("Parsed SRT files. Time elapsed: %s.", time.Since(start))
+
+	start = time.Now()
+	log.Printf("Beginning to count occurrences of 23...")
+	counter := 0
+	for i, sub := range subs {
+		for _, line := range sub.Items {
+			str := line.String()
+			if containsArr(str, twentyThree) && !containsArr(str, blacklist) {
+				fmt.Println(str)
+				fmt.Println(i)
+				fmt.Println()
+				counter++
+			}
+		}
+	}
+
+	log.Printf("Finished counting. Time elapsed: %s", time.Since(start))
+	fmt.Printf("Occurrences found: %d\n", counter)
+}
+
+func containsArr(str string, arr []string) bool {
+	for _, s := range arr {
+		if strings.Contains(str, s) {
+			return true
+		}
+	}
+
+	return false
 }
